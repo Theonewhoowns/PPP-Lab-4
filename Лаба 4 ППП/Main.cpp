3317584dc89415282d1855f379d7e8d940458648
Theonewhoowns <150282882+Theonewhoowns@users.noreply.github.com>
2025-02-24 17:06:25 +0300

    Add files via upload

---
+++ Лаба 4 ППП/Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Functions.h"
+
+using namespace std;
+
+int main() {
+    int choice;
+    
+    do {
+        cout << "Select a program to run:\n";
+        cout << "1. Payroll System\n";
+        cout << "2. Luggage Compartment System\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                menuPayrollSystem();
+                break;
+            case 2:
+                menuLuggageSystem();
+                break;
+            case 0:
+                cout << "Exiting program.\n";
+                break;
+            default:
+                cout << "Invalid choice, try again.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
